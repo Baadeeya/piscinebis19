48105537bb33ec95e86d01be2d6331bf2e41805f
David Gutin <dagutin@shi-r9-p6.s19.be>
2022-09-18 19:30:41 +0200

    added print tab

--- rush01/ex00/ft_utils.c
+++ rush01/ex00/ft_utils.c
@@ -6,7 +6,7 @@
 /*   By: dagutin <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/18 12:49:20 by dagutin           #+#    #+#             */
-/*   Updated: 2022/09/18 18:59:38 by dagutin          ###   ########.fr       */
+/*   Updated: 2022/09/18 19:30:11 by dagutin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,4 +30,31 @@ int	ft_strlen(char *str)
 	while (str[i])
 		i++;
 	return (i);
+}
+
+void	ft_putchar(char c)
+{
+	write (1, &c, 1);
+}
+
+// Ecrit le contenu de tout le tableau caractere par caracteres, separes par des espaces
+// et des retours a la ligne apres chaque serie de 4
+
+void	ft_printtab(char **tab)
+{
+	int	x;
+	int	y;
+
+	x = -1;
+	while (++x < g_size)
+	{
+		y = -1;
+		while (++y < g_size)
+		{
+			write(1, &tab[x][y], 1);
+			if (y != 3)
+				write(1, " ", 1);
+		}
+		write(1, "\n", 1);
+	}
 }
