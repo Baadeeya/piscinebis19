ae16d3be8860cb41d6cd6b7395a9156f7effbe7a
David Gutin <dagutin@shi-r4-p6.s19.be>
2022-09-20 14:56:53 +0200

    done c03 je crois

--- c03/ex05/ft_strlcat.c
+++ c03/ex05/ft_strlcat.c
@@ -1,37 +1,36 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strstr.c                                        :+:      :+:    :+:   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: dagutin <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/09/14 19:03:13 by dagutin           #+#    #+#             */
-/*   Updated: 2022/09/20 14:37:23 by dagutin          ###   ########.fr       */
+/*   Created: 2022/09/20 14:36:37 by dagutin           #+#    #+#             */
+/*   Updated: 2022/09/20 14:56:28 by dagutin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdlib.h>
-
-char	*ft_strstr(char *str, char *to_find)
+unsigned int	ft_strlen(char	*str)
 {
 	unsigned int	i;
-	unsigned int	x;
 
 	i = 0;
-	if (!*to_find)
-		return (str);
 	while (str[i])
-	{
-		x = 0;
-		while (str[i + x] == to_find[x])
-		{
-			if (!(to_find[x]))
-				return (str + i);
-			x++;
-		}
-		if (!(to_find[x]))
-			return (str + i);
 		i++;
-	}
-	return (NULL);
+	return (i);
+}
+
+unsigned int	ft_strlcat(char *dst, char *src, unsigned int size)
+{
+	unsigned int	i;
+	unsigned int	len;
+
+	i = -1;
+	len = ft_strlen(dst);
+	if (size <= len)
+		return (ft_strlen(src) + size);
+	while (src[++i] && i < size - len - 1)
+		dst[len + i] = src[i];
+	dst[len + i] = 0;
+	return (len + ft_strlen(src));
 }
