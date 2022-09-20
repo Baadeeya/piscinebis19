a94f4c19c6f262d555e5e3a6fa7132e4c44507e0
David Gutin <dagutin@shi-r4-p6.s19.be>
2022-09-20 20:01:43 +0200

    done c05

---
+++ c05/ex03/ft_recursive_power.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_recursive_power.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dagutin <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/20 19:55:52 by dagutin           #+#    #+#             */
+/*   Updated: 2022/09/20 19:56:05 by dagutin          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_recursive_power(int nb, int power)
+{
+	if (power < 0)
+		return (0);
+	if (power == 0)
+		return (1);
+	if (power > 0)
+		return (nb * ft_recursive_power(nb, power - 1));
+	else
+		return (nb);
+}
