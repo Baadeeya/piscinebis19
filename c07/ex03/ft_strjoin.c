abc8b02b44dd697717c7764848c698b88e69d4e8
David Gutin <dagutin@shi-r4-p6.s19.be>
2022-09-21 16:12:08 +0200

    strjoin done and starting convert base

--- c07/ex03/ft_strjoin.c
+++ c07/ex03/ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: dagutin <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/21 13:07:01 by dagutin           #+#    #+#             */
-/*   Updated: 2022/09/21 15:22:08 by dagutin          ###   ########.fr       */
+/*   Updated: 2022/09/21 16:09:58 by dagutin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ int	ft_tablen(int size, char **strs, char *sep)
 
 	x = -1;
 	len = 0;
-	while (sep[len])
+	while (sep && sep[len])
 		len++;
 	len *= size - 1;
 	x = -1;
@@ -32,7 +32,7 @@ int	ft_tablen(int size, char **strs, char *sep)
 	}
 	if (len <= 0)
 		return (0);
-	return (len);
+	return (len + 1);
 }
 
 char	*ft_strjoin(int size, char **strs, char *sep)
@@ -43,13 +43,12 @@ char	*ft_strjoin(int size, char **strs, char *sep)
 	int		s;
 	int		i;
 
-	str = malloc(sizeof(char) * ft_tablen(size, strs, sep));
+	str = malloc(ft_tablen(size, strs, sep) * sizeof(char));
 	if (!str)
 		return (NULL);
 	if (!size)
-		return (NULL);
+		return (str);
 	x = -1;
-	s = -1;
 	i = 0;
 	while (++x < size)
 	{
@@ -57,20 +56,9 @@ char	*ft_strjoin(int size, char **strs, char *sep)
 		s = -1;
 		while (strs[x][++y])
 			str[i++] = strs[x][y];
-		while (sep[++s] && x < size - 1)
-		{
-			str[i] = sep[s];
-			i++;
-		}
+		while (sep && sep[++s] && x < size - 1)
+			str[i++] = sep[s];
 	}
 	str[i] = '\0';
 	return (str);
-}
-
-#include <stdio.h>
-
-int	main(int ac, char **av)
-{
-	printf("%s\n", ft_strjoin(ac - 1, av + 1, "BITE"));
-	return (0);
 }
