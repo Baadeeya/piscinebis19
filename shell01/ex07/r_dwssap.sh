7676ba4a767d6e208c1c3288d4c34227b2ac102f
David Gutin <dagutin@shi-r4-p6.s19.be>
2022-09-21 18:16:27 +0200

    shell01 should be good

--- shell01/ex07/r_dwssap.sh
+++
@@ -1,1 +0,0 @@
-cat /etc/passwd | grep -v '#' | awk 'NR%2==0' | cut -d : -f 1 | rev | sort -r | awk 'NR>= ENVRION["FT_LINE1"] && NR<=ENVIRON["FT_LINE2"]' | tr '\n' ' ' | sed '$ s/ $/./' | sed 's/ /, /g' | tr -d '\n'
